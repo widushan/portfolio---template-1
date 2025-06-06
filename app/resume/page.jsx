07d1cad9ac67676a6513b6376ec9ff08695b9103
widushan <114930601+widushan@users.noreply.github.com>
2025-06-06 22:50:11 +0530

    resume_tabs commit
    designing resume tabs

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -3,6 +3,10 @@
 
 import {FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs, FaNextJs, FaFlutter, FaPython, FaPhp, FaCsharp, FaJava, FaCplus, FaTailwindcss} from "react-icons/fa";
 import { SiTailwindcss, SiNextdotjs, SiTypescript } from "react-icons/si";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
+import { motion } from "framer-motion";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 import React from "react";
 
@@ -37,7 +41,8 @@ const about = {
             fieldValue: "English, Sinhala"
         },
     ]
-}
+};
+
 
 
 const experience = {
@@ -62,10 +67,107 @@ const experience = {
         }
 
     ]
-}
+};
+
+
+
+const education = {
+    icon: "/assets/resume/cap.svg",
+    title: "My Education",
+    description: "I hold a Bachelor's degree in Computer Science from the University of Colombo, where I gained a solid foundation in software development principles, algorithms, and data structures. My academic journey has equipped me with the skills necessary to tackle complex programming challenges and develop efficient solutions.",
+    items: [
+        {
+            institution: "SLTC Research University",
+            degree: "BSc in Software Engineering",
+            duration: "2022 - 2026",
+        },
+        {
+            institution: "IIT",
+            degree: "AI/ML Engineering Certification ",
+            duration: "Jun 2025 - Sep 2025"
+        }
+
+    ]
+};
+
+
+
+const skills = {
+    title: "My Skills",
+    description: "I possess a diverse skill set that includes web development, mobile app development, AI/ML, and more. I am proficient in various programming languages and frameworks, allowing me to adapt to different project requirements and deliver high-quality solutions.",
+    skilllist: [
+        {
+            icon: <FaHtml5 />,
+            name: "HTML5",
+        },
+        {
+            icon: <FaCss3 />,
+            name: "CSS3",
+        },
+        {
+            icon: <FaJs />,
+            name: "JavaScript",
+        },
+        {
+            icon: <FaReact />,
+            name: "React",
+        },
+        {
+            icon: <SiNextdotjs />,
+            name: "Next.js",
+        },
+        {
+            icon: <SiTailwindcss />,
+            name: "Tailwind CSS",
+        },
+        {
+            icon: <FaPython />,
+            name: "Python",
+        },
+        {
+            icon: <FaPhp />,
+            name: "PHP",
+        },
+        {
+            icon: <FaCsharp />,
+            name: "C#",
+        },
+        {
+            icon: <FaJava />,
+            name: "Java",
+        },
+        {
+            icon: <FaCplus />,
+            name: "C++",
+        },
+        {
+            icon: <FaFlutter />,
+            name: "Flutter",
+        },
+    ],
+};
+
+
 
 const Resume = () => {
-    return <div>Resume Page</div>
-}
+    return (
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 0.4, delay: 2.4, ease: "easeIn" }, }}
+            className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+        >
+            <div className="container mx-auto">
+                <Tabs defaultValue="about" className="flex flex-col xl:flex-row gap-[60px]">
+                    <TabsList>
+                        <TabsTrigger>About Me</TabsTrigger>
+                        <TabsTrigger>Experience</TabsTrigger>
+                        <TabsTrigger>Education</TabsTrigger>
+                        <TabsTrigger>Skills</TabsTrigger>
+                    </TabsList>
+                </Tabs>
+            </div>
+        </motion.div>
+    );
+};
 
 export default Resume;
