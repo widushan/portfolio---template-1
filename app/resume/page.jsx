1312c41fcaff153d94bc9f1a1c00526519d74ff1
widushan <114930601+widushan@users.noreply.github.com>
2025-06-07 02:28:00 +0530

    skill commit
    designing resume skill section

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -174,12 +174,47 @@ const Resume = () => {
 
 
                         {/*Education*/}
-                        <TabsContent value="education" className="w-full">Education</TabsContent>
+                        <TabsContent value="education" className="w-full">
+                            <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                                <h3 className="text-4xl font-bold">{education.title}</h3>
+                                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{education.description}</p>
+                                <ScrollArea className="h-[400px]">
+                                    <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                                        {education.items.map((item, index) => {
+                                            return (
+                                                <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                                                    <span className="text-accent">{item.duration}</span>
+                                                    <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.degree}</h3>
+                                                    <div className="flex items-center gap-3">
+                                                        <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                                                        <p className="text-white/60">{item.institution}</p>
+                                                    </div>
+                                                </li>
+                                            );
+                                        })}
+                                    </ul>
+                                </ScrollArea>
+                            </div>
+                        </TabsContent>
 
 
 
                         {/*Skills*/}
-                        <TabsContent value="skills" className="w-full">Skills</TabsContent>
+                        <TabsContent value="skills" className="w-full">
+                            <div className="flex flex-col gap-[30px]">
+                                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                                    <h3 className="text-4xl font-bold">{skills.title}</h3>
+                                    <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{skills.description}</p>
+                                </div>
+                                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px]">
+                                    {skills.skilllist.map((skill, index) => {
+                                        return (
+                                            <li key={index}>{skill.name}</li>
+                                        );
+                                    })}
+                                </ul>
+                            </div>
+                        </TabsContent>
 
 
 
