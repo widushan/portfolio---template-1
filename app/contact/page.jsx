26ffc2bb4fc395cbebe3b2376f0cf52e91614529
widushan <114930601+widushan@users.noreply.github.com>
2025-06-07 21:34:58 +0530

    contact commit
    Started to build contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,61 @@
+"use client";
+
 import React from "react";
+
+import { delay, motion } from "framer-motion";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue, SelectGroup,  } from "@/components/ui/select";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa"
+import { Label } from "@/components/ui/label";
+
+const info = [
+    {
+        icon: <FaPhoneAlt className="text-2xl text-primary" />,
+        title: "Phone",
+        description: "+123 456 7890"
+    },
+    {
+        icon: <FaEnvelope className="text-2xl text-primary" />,
+        title: "Email",
+        description: "test1234@gmail.com"
+    },
+    {
+        icon: <FaMapMarkerAlt className="text-2xl text-primary" />,
+        title: "Address",
+        description: "123 Main St, City, Country"
+    }
+];
+
 
 
 const Contact = () => {
-    return <div>Contact Page</div>
-}
+    return (
+        <motion.section
+            initial = {{  opacity: 0 }}
+            animate = {{
+                opacity: 1,
+                transition: { duration: 0.5, delay: 2.4, ease: "easeIn" },
+            }}
+            className = "py-6"
+        >
+            <div className="container mx-auto">
+                <div>
+                    {/*Form*/}
+                    <div>
+                        <form action="">form</form>
+                    </div>
+                    {/*info*/}
+                    <div></div>
+                </div>
+            </div>
+        </motion.section>
+
+    );
+};
+
 
 export default Contact;
