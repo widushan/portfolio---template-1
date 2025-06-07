f9c29f0f28041a6396558819f2d2ec0106af3f30
widushan <114930601+widushan@users.noreply.github.com>
2025-06-07 20:57:47 +0530

    work commit
    Work page done

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -149,7 +149,9 @@ const Work = () => {
                                 )
                             })}
 
-                            <WorkSliderBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none" />
+                            <WorkSliderBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all duration-500" 
+                            /> 
 
                         </Swiper>
                     </div>
