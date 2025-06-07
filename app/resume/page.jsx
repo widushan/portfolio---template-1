11f379089d553626d0e9cffb8c17dbe2b61ab3e9
widushan <114930601+widushan@users.noreply.github.com>
2025-06-07 13:40:32 +0530

    about commit
    about section completed

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -18,7 +18,7 @@ const about = {
     info: [
         {
             fieldName: "Name",
-            fieldValue: "Pasindu Kavishka Widushan"
+            fieldValue: "C.A.P.K Widushan"
         },
         {
             fieldName: "Phone",
@@ -34,7 +34,7 @@ const about = {
         },
         {
             fieldName: "Email",
-            fieldValue: "kavishka.widushan@pm.me"
+            fieldValue: "kavishka2001@gmail.com"
         },
         {
             fieldName: "Languages",
@@ -133,7 +133,22 @@ const Resume = () => {
                     <div className="min-h-[80vh] w-full">
 
                         {/*about*/}
-                        <TabsContent value="about" className="w-full">About Me</TabsContent>
+                        <TabsContent value="about" className="w-full text-center xl:text-left">
+                            <div className="flex flex-col gap-[30px]">
+                                <h3 className="text-4xl font-bold">{about.title}</h3>
+                                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{about.description}</p>
+                                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                                    {about.info.map((item, index) => {
+                                        return (
+                                            <li key={index} className="flex items-center justify-center xl:justify-start gap-3">
+                                                <span className="text-white/60">{item.fieldName}:</span>
+                                                <span className="">{item.fieldValue}</span>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </div>
+                        </TabsContent>
 
 
 
